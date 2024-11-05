11e2058d7a520950621238b9ced4cebbda42ccdf
jorohino <149121790+jorohino@users.noreply.github.com>
2024-11-05 15:05:33 -0500

    Set up App.jsx as root component with routes to Main and SavedNews.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// External Library + Context Imports
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+// Utility Imports
+
+// Primary Component Imports
+
+// Modal Component Imports
+
+// Style Imports
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>What's going on in the world?</h1>
-      <p>
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
-    </>
+    <div className="app">
+      <div className="app__content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/saved-news" element={<SavedNews />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
