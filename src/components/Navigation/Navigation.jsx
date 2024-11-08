7253f1ec9da9ecf9c8e1262e89717debe4abe970
jorohino <149121790+jorohino@users.noreply.github.com>
2024-11-08 16:58:11 -0500

    Implement basic navigation CSS and HTML structure. Reorganize header CSS

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,20 @@
 import { React } from "react";
 import "./Navigation.css";
 
+// TO DO: Contents of navigation__btn-container should change dependent on logged in status:
+//   Logged out/Guest = Home, Sign in
+//   Logged in/User = Home, Saved articles, [username, logout logo]
+
 function Navigation({}) {
-  return <></>;
+  return (
+    <div className="navigation">
+      <h1 className="navigation__title">NewsExplorer</h1>
+      <div className="navigation__btn-container">
+        <button className="navigation__home-btn">Home</button>
+        <button className="navigation__signin-btn">Sign in</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
