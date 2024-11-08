f4df86a44a2c92add71e607fd7d13c0260629a4f
jorohino <149121790+jorohino@users.noreply.github.com>
2024-11-08 17:24:47 -0500

    Implement basic search form CSS and HTML structure. Update header background CSS

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,7 +2,18 @@ import { React } from "react";
 import "./SearchForm.css";
 
 function SearchForm({}) {
-  return <></>;
+  return (
+    <div className="search-form">
+      <input
+        className="search-form__input"
+        type="text"
+        placeholder="Enter topic"
+      ></input>
+      <button className="search-form__submit" type="submit">
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchForm;
