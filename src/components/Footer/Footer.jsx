c2541442240d4b326405777cb8da65315a3625fd
jorohino <149121790+jorohino@users.noreply.github.com>
2024-11-12 13:34:47 -0500

    Implement basic CSS and HTML structure for About section and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,17 @@ import { React } from "react";
 import "./Footer.css";
 
 function Footer({}) {
-  return <></>;
+  return (
+    <div className="footer">
+      <p className="footer__copyright">© 2024 Supersite, Powered by News API</p>
+      <div className="footer__links">
+        <button className="footer__home-btn">Home</button>
+        <button className="footer__t10-btn">TripleTen</button>
+        <button className="footer__gh-btn"></button>
+        <button className="footer__fb-btn"></button>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
